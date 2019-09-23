4a48cef2be98f49bf7700e3c451cae115c68de71
michael <mikelkarije@gmail.com>
2019-09-23 17:15:20 +0300

    this commit adds sqlalchemy to my project

--- manage.py
+++ manage.py
@@ -1,10 +1,22 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
-
+from app.models import User,Post
+from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
-manager=Manager(app)
-
+manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+@manager.command
+def test():
+  
+   import unittest
+   test = unittest.TestLoader().discover('test')
+   unittest.TextTestRunner(verbosity=2).run(test)
+@manager.shell
+def make_shell_context():
+   return dict(app = app , db = db , User = User , Post=Post)
 if __name__ == '__main__':
-    manager.run()
+   manager.run()
+

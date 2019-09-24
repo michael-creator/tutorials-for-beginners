839f0314353b9d3e1c7a65282bfbd3a4b7c5d8ba
michael <mikelkarije@gmail.com>
2019-09-24 14:45:28 +0300

    addition of photos and new data

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = 'asdfghjk98765'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michael:joker@localhost/post'
     
--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from app.models import User,Post
 from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app,db)

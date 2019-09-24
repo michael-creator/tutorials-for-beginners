e7f9e99128edaacaecac3dbc4eac28ad5dd24924
michael <mikelkarije@gmail.com>
2019-09-24 11:17:04 +0300

    this commit deletes the macros html and its files and corrects the auth file views with new data input

--- config.py
+++ config.py
@@ -1,26 +1,25 @@
 import os
 
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michael:joker@localhost/pen'
-#    @staticmethod
-    def init_app(app):
-       pass
 
+    SECRET_KEY = 'asdfghjk98765'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michael:joker@localhost/post'
+    
+    
 
 
 
 class ProdConfig(Config):
-#    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-# class TestConfig(Config):
-#    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michael:joker@localhost/one_test'
-class DevConfig(Config):
-  
-   DEBUG = True
-config_options = {
-   'development' : DevConfig,
-   'production' : ProdConfig,
-   'test':TestConfig
-}
 
+    pass 
+    
+
+class DevConfig(Config):
+
+    DEBUG = True
+
+config_options = {
+'development': DevConfig,
+'production': ProdConfig,
+
+}
